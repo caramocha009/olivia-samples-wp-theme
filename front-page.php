1e287fdbfb640636308a0d1d5f5bafaa176cb09c
Jasmine Francois <francois.jasminem@gmail.com>
2019-11-30 15:21:15 -0600

    Better mobile styles for home and about

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 
 <?php get_header(); ?>
 
-<img class="home" src="<?php echo get_template_directory_uri(); ?>/static/logo.png" alt="Kismet Doula Services" height="auto" width="auto"/>
+<img class="home" src="<?php echo get_template_directory_uri(); ?>/static/logo.png" alt="Kismet Doula Services"/>
 
 <?php get_footer(); ?>
---
+++ partials/page-content.php
@@ -0,0 +1,4 @@
+<div class="page-content">
+    <h2 class="page-title"><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+</div>
