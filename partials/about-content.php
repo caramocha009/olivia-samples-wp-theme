66fd18a40e176858345116ef873e0a2b15c789ec
Michael Parker <michael@parker.gg>
2019-12-27 15:37:53 -0600

    Tweaks for uploading to host

--- partials/about-content.php
+++ partials/about-content.php
@@ -2,5 +2,5 @@
     <h2 class="page-title"><?php the_title(); ?></h2>
     <?php the_content(); ?>
 
-    <a href="<?php echo get_page_link(get_page_by_title('Doula Services')->ID); ?>\"><p>Explore My Services</p></a>
+    <a href="<?php echo get_page_link(get_page_by_title('My Services')->ID); ?>\"><p>Explore My Services</p></a>
 </div>
